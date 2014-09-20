2b547bcc80769a1f0ff2b3882fc2e1423a402b23
Ravi <upman16@gmail.com>
2014-09-20 22:33:52 +0530

    Wrote route to retreive basic student info. Organised datamapper config into separate file

--- app.rb
+++ app.rb
@@ -1,50 +1,11 @@
 require 'bundler'
 Bundler.require
+require_relative './dm_config'
 
-require_relative './models/student'
-require_relative './models/course'
-require_relative './models/department'
-require_relative './models/lecture'
-require_relative './models/absence'
-require_relative './models/exam'
-require_relative './models/score'
-require_relative './models/lectureseries'
-
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{File.dirname(File.expand_path(__FILE__))}/development.sqlite")
-
-class Thing
-	include DataMapper::Resource
-	property :id, Serial, :key => true
-	property :created_at, DateTime
-	property :title, String, :length => 255
-	property :description, Text
-end
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
-if Thing.count == 0
-	Thing.create(:title => "Test Thing One", :description => "Sometimes I eat pizza.")
-	Thing.create(:title => "Test Thing Two", :description => "Other times I eat cookies.")
-end
-
-get '/things/:id' do
-	content_type :json
-	@thing = Thing.get(params[:id].to_i)
-	if @thing
-		@thing.to_json
-	else
-		halt 404
-	end
-end
-
-
-=begin
 not_found do
 	content_type :json
 	halt 404, { error: 'URL not found' }.to_json
 end
-=end
 
 get '/' do
 	content_type :json
@@ -52,21 +13,30 @@ get '/' do
 end
 
 
-get '/students/:srn/?' do
+get '/students/:srn' do
 	content_type :json
-	s = Student.get(params[:srn])
-	if s!=nil
-		s.to_json
+	@s = Student.get(params[:srn])
+	if @s
+		{   
+			srn: @s.srn, 
+			name: @s.full_name, 
+			dob: @s.dob, 
+			semester: @s.semester,
+			course: @s.course.name,
+			department: @s.department.name,
+		}.to_json
 	else
-		{error: "Invalid/Non-existent SRN"}.to_json
+		{
+			error: "Invalid/Non-existent SRN"
+		}.to_json
 	end
 end
 
-get '/students/marks/:srn/?' do
+get '/students/marks/:srn' do
 	content_type :json
-	s = Student.get(params[:srn])
-	if s!=nil
-		s.score.to_json
+	@s = Student.get(params[:srn])
+	if @s
+		@s.score.to_json
 	else
 		{error: "Invalid/Non-existent SRN"}.to_json
 	end
---
+++ dm_config.rb
@@ -0,0 +1,16 @@
+require_relative './models/course'
+require_relative './models/department'
+require_relative './models/lecture_series'
+require_relative './models/enrollment'
+require_relative './models/exam'
+require_relative './models/faculty'
+require_relative './models/score'
+require_relative './models/student'
+require_relative './models/subject'
+require_relative './models/weekly_attendance'
+
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{File.dirname(File.expand_path(__FILE__))}/development.sqlite")
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
